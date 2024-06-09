139aa3237f88c245e80552e54bf74b59041bfda1
azufb <curiousazunas@gmail.com>
2024-06-09 09:49:52 +0900

    service worker

---
+++ src/serviceWorker.js
@@ -0,0 +1,21 @@
+const cacheName = "pwa_sample_practice-v1";
+
+const appShellFiles = [
+  "index.html",
+  "index.tsx",
+  "App.tsx",
+  "index.css",
+  "App.css",
+];
+
+window.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+
+  // 以下のコードが実行されるまでインストールされない
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all: app shell and content");
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
